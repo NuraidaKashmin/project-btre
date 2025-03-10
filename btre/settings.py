f9d1053f803b07084728c700623c7037c7907b52
Nuraida Kashmin <nuraidakkashmin@gmail.com>
2025-03-10 21:51:00 +0600

    made dynamic dashboard

--- btre/settings.py
+++ btre/settings.py
@@ -144,6 +144,12 @@ MESSAGE_TAGS = {
 }
 
 
+# email config
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
 
 
 # Default primary key field type
